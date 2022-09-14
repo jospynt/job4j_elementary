49ca467d426bc3db4bccf710c46b8beaa83ebd90
user <acula3935@gmail.com>
2022-09-14 14:38:24 +0300

    5.5. Простое число [#171694 #343017]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number != 1;
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                prime = false;
-                break;
+        boolean prime = number > 1;
+        if (prime) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    prime = false;
+                    break;
+                }
             }
         }
         return prime;
